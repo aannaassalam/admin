e9934efaabf7b4908d76cce28c32e51b037d2805
Anas Alam <anasalam027@gmail.com>
2023-01-17 11:50:17 +0530

    minor changes in admin

--- src/layout/pages/orders/orders.js
+++ src/layout/pages/orders/orders.js
@@ -31,125 +31,127 @@ function Orders() {
   return (
     <div className="orders">
       <div className="table">
-        {state?.orders?.map((item, id) => {
-          return (
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-              >
-                <div className="order">
-                  <p style={{ justifySelf: "flex-start" }} className="col-p">
-                    <span>Name</span>
-                    {item?.shipping_address.name}
-                  </p>
-                  <p className="col-p">
-                    <span>Phone</span>
-                    {item?.user.user_phone}
-                  </p>
-                  <p className="col-p">
-                    <span>Order ID</span>
-                    {item?.id}
-                  </p>
-                  <p className="col-p">
-                    <span>Order Total</span>${item?.total}
-                  </p>
-                  <p className="col-p">
-                    <span>Date</span>
-                    {moment(item?.date.toDate()).fromNow()}
-                  </p>
-                  <p className="col-p" style={{ alignItems: "center" }}>
-                    <span>Actions</span>
-                    <div className="buttons">
-                      <button>Accept</button>
-                      <button>Decline</button>
-                    </div>
-                  </p>
-                </div>
-              </AccordionSummary>
-              <AccordionDetails>
-                <div className="top">
-                  <div>
-                    <h4>Shipping Address</h4>
+        {state?.orders
+          ?.sort((a, b) => b.date - a.date)
+          .map((item, id) => {
+            return (
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <div className="order">
+                    <p style={{ justifySelf: "flex-start" }} className="col-p">
+                      <span>Name</span>
+                      {item?.shipping_address.name}
+                    </p>
+                    <p className="col-p">
+                      <span>Phone</span>
+                      {item?.user.user_phone}
+                    </p>
+                    <p className="col-p">
+                      <span>Order ID</span>
+                      {item?.id}
+                    </p>
+                    <p className="col-p">
+                      <span>Order Total</span>${item?.total}
+                    </p>
+                    <p className="col-p">
+                      <span>Date</span>
+                      {moment(item?.date.toDate()).fromNow()}
+                    </p>
+                    <p className="col-p" style={{ alignItems: "center" }}>
+                      <span>Actions</span>
+                      <div className="buttons">
+                        <button>Accept</button>
+                        <button>Decline</button>
+                      </div>
+                    </p>
+                  </div>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <div className="top">
                     <div>
-                      <p>
-                        <div className="col-p">
-                          Address 1: {item.shipping_address.address1} ,{" "}
-                        </div>
-                        <p className="col-p">
-                          <span>City</span>
-                          {item.shipping_address.city}{" "}
+                      <h4>Shipping Address</h4>
+                      <div>
+                        <p>
+                          <div className="col-p">
+                            Address 1: {item.shipping_address.address1} ,{" "}
+                          </div>
+                          <p className="col-p">
+                            <span>City</span>
+                            {item.shipping_address.city}{" "}
+                          </p>
+                          <p className="col-p">
+                            <span>State</span>
+                            {item.shipping_address.state}{" "}
+                          </p>
+                          <p className="col-p">
+                            <span>Zipcode</span>
+                            {item.shipping_address.zipcode}
+                          </p>
                         </p>
-                        <p className="col-p">
-                          <span>State</span>
-                          {item.shipping_address.state}{" "}
-                        </p>
-                        <p className="col-p">
-                          <span>Zipcode</span>
-                          {item.shipping_address.zipcode}
-                        </p>
-                      </p>
-                      <p> Address 2: {item.shipping_address.address2}</p>
+                        <p> Address 2: {item.shipping_address.address2}</p>
+                      </div>
                     </div>
-                  </div>
-                  <div>
-                    <h4>Billing Address</h4>
                     <div>
-                      <p>
-                        Address 1 :{item.billing_address.address1}{" "}
-                        <p className="col-p">
-                          <span>City</span>
-                          {item.billing_address.city}{" "}
+                      <h4>Billing Address</h4>
+                      <div>
+                        <p>
+                          Address 1 :{item.billing_address.address1}{" "}
+                          <p className="col-p">
+                            <span>City</span>
+                            {item.billing_address.city}{" "}
+                          </p>
+                          <p className="col-p">
+                            <span>State</span>
+                            {item.billing_address.state}{" "}
+                          </p>
+                          <p className="col-p">
+                            <span>Zipcode</span>
+                            {item.billing_address.zipcode}
+                          </p>
                         </p>
-                        <p className="col-p">
-                          <span>State</span>
-                          {item.billing_address.state}{" "}
-                        </p>
-                        <p className="col-p">
-                          <span>Zipcode</span>
-                          {item.billing_address.zipcode}
-                        </p>
-                      </p>
-                      <p> Address 2 : {item.billing_address.address2}</p>
+                        <p> Address 2 : {item.billing_address.address2}</p>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <h4>Order Items</h4>
-                {item.items.map((prod) => (
-                  <div className="orderItem">
-                    <p className="col-p">
-                      <span>Product Name</span>
-                      <a
-                        href={`https://nutuyu72.web.app/product/${prod.id}`}
-                        target="_blank"
-                        rel="noreferrer"
-                        style={{ textDecoration: "underline" }}
-                      >
-                        {prod.name}
-                      </a>
-                    </p>
-                    <p className="col-p">
-                      <span>Product Category</span>
-                      {prod.category}
-                    </p>
-                    <p className="col-p">
-                      <span>Product SubCategory</span>
-                      {prod.subcategory.name}
-                    </p>{" "}
-                    <p className="col-p">
-                      <span>Product Type</span>
-                      {prod.subcategory.type && prod.subcategory.type}
-                    </p>
-                    <p className="col-p">
-                      <span>Product Price</span>${prod.price}
-                    </p>
-                  </div>
-                ))}
-              </AccordionDetails>
-            </Accordion>
-          );
-        })}
+                  <h4>Order Items</h4>
+                  {item.items.map((prod) => (
+                    <div className="orderItem">
+                      <p className="col-p">
+                        <span>Product Name</span>
+                        <a
+                          href={`https://nutuyu72.web.app/product/${prod.id}`}
+                          target="_blank"
+                          rel="noreferrer"
+                          style={{ textDecoration: "underline" }}
+                        >
+                          {prod.name}
+                        </a>
+                      </p>
+                      <p className="col-p">
+                        <span>Product Category</span>
+                        {prod.category}
+                      </p>
+                      <p className="col-p">
+                        <span>Product SubCategory</span>
+                        {prod.subcategory.name}
+                      </p>{" "}
+                      <p className="col-p">
+                        <span>Product Type</span>
+                        {prod.subcategory.type && prod.subcategory.type}
+                      </p>
+                      <p className="col-p">
+                        <span>Product Price</span>${prod.price}
+                      </p>
+                    </div>
+                  ))}
+                </AccordionDetails>
+              </Accordion>
+            );
+          })}
       </div>
     </div>
   );
